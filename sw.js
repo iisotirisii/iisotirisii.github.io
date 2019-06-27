9faf34efa002b53727fbf4d36863a54ad7cc6a5d
iisotirisii <52073637+iisotirisii@users.noreply.github.com>
2019-06-27 15:04:42 +0530

    ✅

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 //     All Rights Reserved - https://github.com/AakashPandey/skyroutes
 
 
-const chng="rewired-x", vn="rel-1.1";
+const chng="rewired", vn="rel-1.2";
 var appCash=[
     "/index.html",
     "/lib/skyroute.css",
